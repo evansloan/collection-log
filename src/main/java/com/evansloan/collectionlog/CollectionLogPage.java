ba1adec7d2496a51b27f95e7da74266fd85f53c6
call-me-maple <85463994+call-me-maple@users.noreply.github.com>
2022-09-28 22:17:16 -0500

    add toa alias ignore capitalization when looking up a page remove redundant aliases

--- src/main/java/com/evansloan/collectionlog/CollectionLogPage.java
+++ src/main/java/com/evansloan/collectionlog/CollectionLogPage.java
@@ -22,48 +22,34 @@ public class CollectionLogPage
         {
             // Bosses Tab
             case "sire":
-            case "abyssal sire":
                 return "Abyssal Sire";
             case "hydra":
-            case "alchemical hydra":
                 return "Alchemical Hydra";
             case "byro":
-            case "bryophyta":
                 return "Bryophyta";
             case "barrows":
-            case "barrows chests":
                 return "Barrows Chests";
-            case "callisto":
+            case "cal":
                 return "Callisto";
-            case "cerberus":
             case "cerb":
                 return "Cerberus";
-            case "chaos elemental":
             case "chaos ele":
                 return "Chaos Elemental";
-            case "chaos fanatic":
-                return "Chaos Fanatic";
-            case "commander zilyana":
             case "sara":
             case "saradomin":
             case "zilyana":
             case "zily":
                 return "Commander Zilyana";
-            case "corporeal beast":
             case "corp":
                 return "Corporeal Beast";
-            case "crazy archaeologist":
             case "crazy arch":
                 return "Crazy Archaeologist";
-            case "dagannoth kings":
             case "dks":
                 return "Dagannoth Kings";
-            case "the fight caves":
             case "jad":
             case "fight caves":
             case "fc":
                 return "The Fight Caves";
-            case "the gauntlet":
             case "gaunt":
             case "gauntlet":
             case "cgaunt":
@@ -71,51 +57,40 @@ public class CollectionLogPage
             case "the corrupted gauntlet":
             case "cg":
                 return "The Gauntlet";
-            case "giant mole":
             case "mole":
                 return "Giant Mole";
-            case "general graardor":
             case "bando":
             case "bandos":
             case "graardor":
                 return "General Graardor";
-            case "grotesque guardians":
             case "dusk":
             case "dawn":
             case "gargs":
             case "ggs":
             case "gg":
                 return "Grotesque Guardians";
-            case "hespori":
+            case "farming guild":
+            case "uim":
                 return "Hespori";
-            case "the inferno":
             case "zuk":
             case "inferno":
                 return "The Inferno";
-            case "kalphite queen":
             case "kq":
                 return "Kalphite Queen";
-            case "king black dragon":
             case "kbd":
                 return "King Black Dragon";
-            case "kraken":
             case "cave kraken":
                 return "Kraken";
-            case "kree'arra":
             case "arma":
             case "kree":
             case "kreearra":
             case "armadyl":
                 return "Kree'arra";
-            case "k'ril tsutsaroth":
             case "zammy":
             case "zamorak":
             case "kril":
             case "kril trutsaroth":
                 return "K'ril Tsutsaroth";
-            case "nex":
-                return "Nex";
-            case "the nightmare":
             case "nightmare":
             case "nm":
             case "tnm":
@@ -127,39 +102,43 @@ public class CollectionLogPage
             case "phosani nightmare":
             case "phosanis nightmare":
                 return "The Nightmare";
-            case "obor":
+            case "hill giant":
                 return "Obor";
-            case "sarachnis":
+            case "saracha":
+            case "sarac":
+            case "saracnis":
                 return "Sarachnis";
-            case "scorpia":
+            case "scorpa":
                 return "Scorpia";
-            case "skotizo":
+            case "skotizos":
+            case "totems":
+            case "totem":
                 return "Skotizo";
             case "tempoross":
             case "temp":
                 return "Tempoross";
-            case "thermonuclear smoke devil":
             case "smoke devil":
+            case "smoke devils":
             case "thermy":
+            case "thermey":
                 return "Thermonuclear Smoke Devil";
-            case "venenatis":
             case "vene":
                 return "Venenatis";
-            case "vet'ion":
             case "vetion":
+            case "vet ion":
+            case "veti":
                 return "Vet'ion";
-            case "vorkath":
             case "vork":
+            case "vorki":
                 return "Vorkath";
-            case "wintertodt":
             case "wt":
             case "todt":
                 return "Wintertodt";
-            case "zalcano":
             case "zalc":
                 return "Zalcano";
-            case "zulrah":
             case "zul":
+            case "profit snake":
+            case "bjs":
                 return "Zulrah";
 
             // Raids Tab
@@ -184,7 +163,7 @@ public class CollectionLogPage
             case "tob sm":
             case "tob story mode":
             case "tob story":
-            case "Theatre of Blood: Entry Mode":
+            case "theatre of blood: entry mode":
             case "tob em":
             case "tob entry mode":
             case "tob entry":
@@ -195,42 +174,58 @@ public class CollectionLogPage
             case "tob hard":
             case "hmt":
                 return "Theatre of Blood";
+            case "toa":
+            case "tomb":
+            case "tombs":
+            case "raids 3":
+            case "tomb of amascut":
+                return "Tombs of Amascut";
 
             // Clues Tab
-            case "beginner treasure trails":
             case "begs":
             case "beg clues":
             case "beginners":
             case "beginner clues":
             case "beginner clue":
+            case "clues beg":
+            case "clues beginner":
+            case "clue beg":
+            case "clue beginner":
                 return "Beginner Treasure Trails";
-            case "easy treasure trails":
             case "easy clues":
             case "easy clue":
             case "easies":
+            case "clues easy":
+            case "clue easy":
                 return "Easy Treasure Trails";
-            case "medium treasure trails":
             case "meds":
+            case "med":
             case "mediums":
             case "medium clues":
             case "medium clue":
+            case "clues med":
+            case "clues medium":
+            case "clue med":
+            case "clue medium":
                 return "Medium Treasure Trails";
-            case "hard treasure trails":
             case "hards":
             case "hard clues":
             case "hard clue":
+            case "clue hard":
+            case "clues hard":
                 return "Hard Treasure Trails";
-            case "master treasure trails":
             case "masters":
             case "master clues":
             case "master clue":
+            case "clue master":
+            case "clues master":
                 return "Master Treasure Trails";
-            case "elite treasure trails":
             case "elites":
             case "elite clues":
             case "elite clue":
+            case "clue elite":
+            case "clues elite":
                 return "Elite Treasure Trails";
-            case "hard treasure trails (rare)":
             case "hards rare":
             case "hard clues rare":
             case "hard clue rare":
@@ -240,7 +235,6 @@ public class CollectionLogPage
             case "rare hards":
             case "rare hard":
                 return "Hard Treasure Trails (Rare)";
-            case "elite treasure trails (rare)":
             case "elites rare":
             case "elite clues rare":
             case "elite clue rare":
@@ -250,7 +244,6 @@ public class CollectionLogPage
             case "rare elite":
             case "rare elites":
                 return "Elite Treasure Trails (Rare)";
-            case "master treasure trails (rare)":
             case "master treasure trails rare":
             case "masters rare":
             case "master clues rare":
@@ -262,7 +255,6 @@ public class CollectionLogPage
             case "rare master":
             case "rare masters":
                 return "Master Treasure Trails (Rare)";
-            case "shared treasure trail rewards":
             case "shared rewards":
             case "shared clue":
             case "shared clues":
@@ -270,32 +262,26 @@ public class CollectionLogPage
                 return "Shared Treasure Trail Rewards";
 
             // Minigames Tab
-            case "barbarian assault":
             case "ba":
             case "barb":
                 return "Barbarian Assault";
-            case "brimhaven agility arena":
             case "brimhaven agility":
             case "brimhaven":
             case "brim agility":
             case "brim":
                 return "Brimhaven Agility Arena";
-            case "castle wars":
             case "cw":
             case "cwars":
                 return "Castle Wars";
-            case "fishing trawler":
             case "trawler":
                 return "Fishing Trawler";
-            case "giants' foundry":
             case "giants foundry":
             case "foundry":
+            case "gf":
                 return "Giants' Foundry";
-            case "gnome restaurant":
             case "gnome":
             case "restaurant":
                 return "Gnome Restaurant";
-            case "guardians of the rift":
             case "guardian of the rift":
             case "gotr":
             case "grift":
@@ -303,85 +289,81 @@ public class CollectionLogPage
             case "guardians":
             case "runetodt":
                 return "Guardians of the Rift";
-            case "hallowed sepulchre":
             case "hs":
+            case "sepc":
             case "sepulchre":
                 return "Hallowed Sepulchre";
-            case "last man standing":
             case "lms":
                 return "Last Man Standing";
-            case "magic training arena":
             case "mta":
+            case "magic arena":
                 return "Magic Training Arena";
-            case "mahogany homes":
             case "mah homes":
+            case "mahogany home":
             case "mh":
                 return "Mahogany Homes";
-            case "pest control":
             case "pc":
                 return "Pest Control";
-            case "rogues' den":
             case "rogues den":
             case "rogue den":
             case "rogues":
             case "rogue":
                 return "Rogues' Den";
-            case "shades of mort'ton":
             case "shades of mortton":
+            case "shade of mortton":
             case "shades":
+            case "shade":
+            case "mort ton":
+            case "mortton":
                 return "Shades of Mort'ton";
-            case "soul wars":
             case "soul war":
             case "sw":
                 return "Soul Wars";
-            case "temple trekking":
             case "trekking":
+            case "temple trek":
                 return "Temple Trekking";
-            case "tithe farm":
             case "tithe":
                 return "Tithe Farm";
-            case "trouble brewing":
             case "brewing":
                 return "Trouble Brewing";
-            case "volcanic mine":
             case "vm":
+            case "volc mine":
                 return "Volcanic Mine";
 
             // Other Tab
-            case "aerial fishing":
             case "aerial fish":
+            case "aerial":
                 return "Aerial Fishing";
             case "all pets":
             case "pets":
             case "pet":
                 return "All Pets";
-            case "camdozaal":
-                return "Camdozaal";
-            case "champion's challenge":
             case "champions challenge":
             case "champion challenge":
             case "champion scrolls":
             case "champ scrolls":
                 return "Champion's Challenge";
-            case "chaos druids":
+            case "chaos druid":
+            case "elder druids":
+            case "elder druid":
+            case "elder":
                 return "Chaos Druids";
-            case "chompy bird hunting":
             case "chompy":
             case "chompies":
+            case "bird hunting":
                 return "Chompy Bird Hunting";
-            case "creature creation":
             case "tower of life":
                 return "Creature Creation";
-            case "cyclopes":
             case "defenders":
             case "defender":
                 return "Cyclopes";
-            case "fossil island notes":
             case "fossil island note":
             case "fossil island":
                 return "Fossil Island Notes";
-            case "glough's experiments":
             case "gloughs experiments":
+            case "gloughs experiment":
+            case "glough experiments":
+            case "glough experiment":
             case "demonics":
             case "demonic gorilla":
             case "demonic gorillas":
@@ -394,44 +376,45 @@ public class CollectionLogPage
                 return "Monkey Backpacks";
             case "motherlode mine":
             case "motherlode":
+            case "mlm":
             case "mm":
                 return "Motherlode Mine";
             case "my notes":
             case "notes":
+            case "note":
                 return "My Notes";
-            case "random events":
             case "random event":
             case "random":
             case "randoms":
                 return "Random Events";
-            case "revenants":
             case "revs":
                 return "Revenants";
-            case "rooftop agility":
             case "rooftop":
             case "rooftops":
+            case "agility":
             case "graceful":
                 return "Rooftop Agility";
-            case "shayzien armour":
             case "shayzien":
                 return "Shayzien Armour";
-            case "shooting stars":
             case "shooting star":
             case "star mining":
             case "ss":
+            case "stars":
                 return "Shooting Stars";
-            case "skilling pets":
             case "skilling pet":
             case "skill pets":
             case "skill pet":
+            case "pets skill":
+            case "pet skill":
+            case "pets skilling":
+            case "pet skilling":
                 return "Skilling Pets";
-            case "slayer":
+            case "slay":
                 return "Slayer";
-            case "tzhaar":
+            case "tz haar":
             case "tz":
             case "tzh":
                 return "TzHaar";
-            case "miscellaneous":
             case "misc":
                 return "Miscellaneous";
 
