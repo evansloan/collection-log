b820ed543930de9762787d1a301357cd86362769
Evan Sloan <evan@evans.loan>
2023-04-09 14:06:00 -0400

    save is updated on json serialize

--- src/main/java/com/evansloan/collectionlog/CollectionLogPage.java
+++ src/main/java/com/evansloan/collectionlog/CollectionLogPage.java
@@ -1,16 +1,16 @@
 package com.evansloan.collectionlog;
 
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import net.runelite.api.widgets.Widget;
-import net.runelite.client.util.Text;
+import com.google.common.collect.ImmutableMap;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.List;
 import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class CollectionLogPage
 {
 	private static final Map<String, Predicate<CollectionLogItem>> ITEM_FILTERS = new ImmutableMap.Builder<String, Predicate<CollectionLogItem>>()
@@ -19,17 +19,12 @@ public class CollectionLogPage
 		.put("dupes", (item) -> item.getQuantity() > 1)
 		.build();
 
-    @NonNull
     private final String name;
-
-    @NonNull
     private List<CollectionLogItem> items;
-
-    @NonNull
     private final List<CollectionLogKillCount> killCounts;
 
     @Setter
-    private transient boolean isUpdated = false;
+    private boolean isUpdated;
 
     public static String aliasPageName(String pageName)
     {
@@ -443,39 +438,6 @@ public class CollectionLogPage
         return (int) items.stream()
             .filter(CollectionLogItem::isObtained)
             .count();
-    }
-
-    public void updateItems(Map<String, Widget> itemsToUpdate)
-    {
-        items.clear();
-
-        for (Map.Entry<String, Widget> itemToUpdate : itemsToUpdate.entrySet())
-        {
-            String itemName = itemToUpdate.getKey();
-            Widget item = itemToUpdate.getValue();
-            boolean isObtained = item.getOpacity() == 0;
-
-            items.add(new CollectionLogItem(
-                item.getItemId(),
-                itemName,
-                item.getItemQuantity(),
-                isObtained,
-                items.size()
-            ));
-        }
-    }
-
-    public void updateKillCounts(List<String> killCountStrings)
-    {
-        killCounts.clear();
-        for (String killCountString : killCountStrings)
-        {
-            String[] killCountSplit = killCountString.split(": ");
-            String name = killCountSplit[0];
-            String amount = Text.removeTags(killCountSplit[1])
-                .replace(",", "");
-            killCounts.add(new CollectionLogKillCount(name, Integer.parseInt(amount)));
-        }
     }
 
     public CollectionLogItem getItemById(int itemId)
