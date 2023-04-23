5235a89813105f6d2bd9a86d18c413da9dc3b5d2
Evan Sloan <evan@evans.loan>
2023-04-23 19:20:31 -0400

    update plugin version

--- src/main/java/com/evansloan/collectionlog/CollectionLogConfig.java
+++ src/main/java/com/evansloan/collectionlog/CollectionLogConfig.java
@@ -10,7 +10,7 @@ import net.runelite.client.config.ConfigSection;
 @ConfigGroup("collectionlog")
 public interface CollectionLogConfig extends Config
 {
-	String PLUGIN_VERSION = "2.4.0";
+	String PLUGIN_VERSION = "3.0.0";
 
 	Color DEFAULT_GREEN = new Color(13, 193, 13);
 	Color DEFAULT_ORANGE = new Color(255, 152, 31);
